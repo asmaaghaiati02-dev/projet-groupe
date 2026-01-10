fe172465132b2f47c08642a1a854d9afce5a9b09
asmaaghaiati02-dev <asmaaghaiati02@gmail.com>
2026-01-10 13:43:47 +0100

    laisse comme cest

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-function showSection(sectionId) {
-  document.querySelectorAll("section").forEach(section => {
-    section.style.display = "none";
-  });
 
-  document.getElementById(sectionId).style.display = "block";
-}
